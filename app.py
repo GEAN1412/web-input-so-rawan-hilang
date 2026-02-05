3b4b014c23f012d913824690d1a27396ae5be455
GEAN1412 <authenticcase1@gmail.com>
2026-02-05 10:31:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -352,8 +352,8 @@ elif st.session_state.page == "ADMIN":
                         st.download_button("📥 Download Rekap", buf.getvalue(), f"Rekap_SO_{get_indonesia_date()}.xlsx")
             st.divider()
             if st.button("🗑️ Hapus Inputan Lama", use_container_width=True):
-                if v_now: confirm_delete_old_data(v_now)
-
+                if v_now: confirm_delete_old_data(v_now) # Sekarang v_now pasti aman
+                else: st.error("Master tdk ditemukan.")
         with t2:
             logs = load_json_db(LOG_DB_PATH)
             if logs:
@@ -394,3 +394,4 @@ elif st.session_state.page == "USER_INPUT":
                 c_fisik = next((c for c in data_input.columns if 'fisik' in c.lower()), 'Jml Fisik')
                 c_selisih = next((c for c in data_input.columns if 'selisih' in c.lower()), 'Selisih')
                 show_user_editor(data_input, c_sales, c_fisik, c_stok, c_selisih, st.session_state.active_toko, v_now)
+
